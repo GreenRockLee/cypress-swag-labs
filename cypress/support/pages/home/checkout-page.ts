e220b8ee8c1de5107d912a5b3a588edcf28f9e69
“Matúš <matus.sasik@gmail.com>
2026-02-13 14:27:10 +0100

    update for checkout form

--- cypress/support/pages/home/checkout-page.ts
+++ cypress/support/pages/home/checkout-page.ts
@@ -6,4 +6,35 @@ export class CheckoutPage {
         postalCodeInput: () => cy.getByTestId('postalCode'),
         continueButton: () => cy.getByTestId('continue'),
     };
+
+    /**
+     * Fills out the checkout form with the provided information
+     */
+    fillCheckoutForm(firstName: string, lastName: string, postalCode: string) {
+        //First name input
+        this.elements.firstNameInput()
+        .should('be.visible');
+        this.elements.firstNameInput()
+        .type(firstName);
+        this.elements.firstNameInput()
+        .should('have.value', firstName);
+
+        // Last name input
+        this.elements.lastNameInput()
+        .should('be.visible');
+        this.elements.lastNameInput()
+        .type(lastName);
+        this.elements.lastNameInput()
+        .should('have.value', lastName);
+
+        // Postal code input
+        this.elements.postalCodeInput()
+        .should('be.visible');
+        this.elements.postalCodeInput()
+        .type(postalCode);
+        this.elements.postalCodeInput()
+        .should('have.value', postalCode);
+
+        return this;
+    }
 }
